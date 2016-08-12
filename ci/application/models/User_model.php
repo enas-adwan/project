4cabb933153e4053134b0c5fd772234ce0060aa6
enas-adwan <eodwan@eodwan.home>
2016-08-12 16:40:39 +0300

    try

--- ci/application/models/User_model.php
+++ ci/application/models/User_model.php
@@ -317,7 +317,7 @@ $id=$this->session->id;
 					$this->db->select('*');
 					$this->db->from('articles');
 					$this->db->join('reg', 'reg.id = articles.id');
-			//		$this->db->join('imagees', 'imagees.id_article = articles.id_article');
+			//	$this->db->join('imagees', 'imagees.id_article = articles.id_article');
 				$this->db->where('reg.id', $id);
 						//$this->db->select('title, body,id_article ,id,slug');
 
--- ci/application/views/view.php
+++ ci/application/views/view.php
@@ -12,7 +12,7 @@
 
   }
 
-$image=$news_item['image'];
+//$image=$news_item['image'];
 
 //  echo "<br /><br /><img  id='previewing' width='200' height='200' src='".base_url($image)."' alt='Profile Pic'>";
 echo '<h2>'.$news_item['title'].'</h2>';
